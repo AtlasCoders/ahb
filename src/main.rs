b03e91f72bf73815189a0e55bdc19b1dc9afbe44
Colere-Sys <anashassari08@gmail.com>
2023-05-29 11:39:08 +0200

    adding parameters and parsing parameters using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    url: String,
+
+    #[arg(short, long, default_value_t = 80)]
+    port: u8,
+
+    #[arg(short, long, default_value = "GET")]
+    method: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    requests: u8,
+
+    #[arg(short, long, default_value_t = 1)]
+    concurrency: u8,
+}
+
 fn main() {
-    println!("Atlas Coders Http benchmarker, world!");
+    let args = Args::parse();
+
+    
 }
