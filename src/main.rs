907a682f914c15613e0ad11a5d0d82b5875c00eb
0uss-ben <oussamabendaou63@gmail.com>
2023-05-29 19:29:02 +0100

    Adding a HTTP methods

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use serde::{Deserialize, Serialize};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -18,6 +19,57 @@ struct Args {
     concurrency: u8,
 }
 
-fn main() {
-    let args = Args::parse();
+
+
+//Struct for JSON FORMAT uncomment to use
+// #[derive(Serialize, Deserialize, Debug)]
+// struct PersonResponse {
+//     id: i32,
+//     username: String,
+//     email: String,
+//     firstName: String,
+//     lastName: String,
+//     gender: String,
+//     image: String,
+//     token: String,
+// }
+
+pub async fn get_method(url : String) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let body = client.get(url).send()
+	    .await?
+        .text()
+        .await?;
+    Ok(body)
+}
+
+pub async fn post_method(url : &str,json_data : &str) -> Result<String, Box<dyn std::error::Error>> {
+
+    // let json_data = r#"{"username": "kminchelle", "password": "0lelplR"}"#;//JSON DATA
+
+    let client = reqwest::Client::new();
+
+
+    let res = client.post(url)
+    .header("Content-Type","application/json")
+    .body(json_data.to_owned())
+    .send()
+    .await?;
+
+    //JSON deserialization uncomment to use
+    //  let i : PersonResponse = res.json::<PersonResponse>().await.unwrap();
+    //  println!("{:?}",i.id);//id
+    //  println!("{:?}",i.username);//username
+    //  println!("{:?}",i.email);//email
+    
+    Ok(res.text().await?)
+}
+
+#[tokio::main]
+async fn main() {
+    /let args = Args::parse();
+    //POST METHOD TEST
+    // let data = r#"{"username": "kminchelle", "password": "0lelplR"}"#;
+	//  let a =post_method("https://dummyjson.com/auth/login",data).await;
+    //  println!("{:?}",a);
 }
